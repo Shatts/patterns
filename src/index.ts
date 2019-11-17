f106cdc09341a8f73e51fbc21b83922c34c67478
kristina.shatts@gmail.com <&m3]QmAfh.([=X>
2019-11-17 23:56:18 +0200

    Bridge pattern

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
 import { TextMessageSender } from './bridge/implementaton/text-message-sender';
 import { EmailMessageSender } from './bridge/implementaton/email-message-sender';
-import { TelegramMessageSender } from './bridge/implementaton/telegram-message-sender';
 import { TextMessage } from './bridge/abstraction/text-message';
 import { EmailMessage } from './bridge/abstraction/email-message';
 
-/*const textMessageSender = new TextMessageSender();
+const textMessageSender = new TextMessageSender();
 const textMessage = new TextMessage(textMessageSender);
-textMessage.send({to: '+380956848517', message: 'testing...'});*/
+textMessage.send({to: '+380956848517', message: 'testing...'});
 
-/*const emailMessageSender = new EmailMessageSender();
+const emailMessageSender = new EmailMessageSender();
 const emailMessage = new EmailMessage(emailMessageSender);
-emailMessage.send({to: 'kristina.shatts@gmail.com', subject: 'test', message: 'testing...'});*/
+emailMessage.send({to: 'kristina.shatts@gmail.com', subject: 'test', message: 'testing...'});
